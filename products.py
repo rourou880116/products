3efa5206bdcd48b20a8da0bbee6fe717271fb6a5
rourou <rourou880116@gmail.com>
2023-06-08 21:34:34 +0800

    inspect file

--- products.py
+++ products.py
@@ -1,22 +1,34 @@
+import os
 products = []
-while  True:
+if os.path.isfile('products.csv'):    #檢查檔案在不在
+    print('找到檔案了!')
+    #讀取檔案
+    with open('products.csv', 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('找不到檔案.....')
+
+#讓使用者輸入
+while True:
     name = input('請輸入商品名稱: ')
-    if name == "q":
+    if name == 'q':
         break
-    price = input('清輸入商品價格: ')
-    # p = []
-    # p.append(name)
-    # p.append(price)
-    # products.append(p)
-    products.append([name,price])
+    price = input('請輸入價格: ')
+    price = int(price)
+    products.append([name, price])
 print(products)
 
-print(products[0][0],products[0][1])
-
+#印出所有購買紀錄
 for p in products:
-	print(p[0],'的價格是: ',p[1])
+    print(ｐ[0], '的價格是: ', p[1])
 
+#寫入檔案csv
 with open('products.csv', 'w', encoding= 'utf-8') as f:
     f.write('商品,價格\n')
     for p in products:
-        f.write( p[0] + ',' + p[1] + '\n')
+        f.write(p[0]+ ','+ str(p[1])+ '\n')
